5f1d8eef99e6a0486976ccd1293f5fc8da09a2a5
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-08-06 16:56:23 -0300

    Adiciona Pagina de cadastro do trabalhador

--- zelo-paginas/src/app/app-routing.module.ts
+++ zelo-paginas/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'cadastro-trabalhador',
+    loadChildren: () => import('./cadastro-trabalhador/cadastro-trabalhador.module').then( m => m.CadastroTrabalhadorPageModule)
   },
+
 ];
 
 @NgModule({
---
+++ zelo-paginas/src/app/cadastro-trabalhador/cadastro-trabalhador.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-trabalhador',
+  templateUrl: './cadastro-trabalhador.page.html',
+  styleUrls: ['./cadastro-trabalhador.page.scss'],
+})
+export class CadastroTrabalhadorPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
