5d518dddf1d8bec5147218d1348a88beea59fff9
João Sena <joaosena0206@gmail.com>
2024-08-10 16:59:32 -0300

    Permite esconder e mostrar a senha

--- zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
+++ zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, input, OnInit, ViewChild } from '@angular/core';
+import { IonIcon } from '@ionic/angular';
 
 @Component({
     selector: 'app-cadastro-cliente',
     templateUrl: './cadastro-cliente.page.html',
     styleUrls: ['./cadastro-cliente.page.scss'],
 })
-export class CadastroClientePage implements OnInit {    
+export class CadastroClientePage implements OnInit {
     date: any;
-    teste: any;
     
     constructor() {
+        
     }
 
     ngOnInit() {
+    }
 
+    ngAfterViewInit()
+    {
+    
+    }
+
+    estadoSenha(event: any)
+    {
+        const olho = event.target;
+        const input = event.target.parentElement
+
+        if (input.type == "password")
+        {
+            olho.name = "eye-off-outline";
+            input.type = "text";
+        }
+        else
+        {
+            olho.name = "eye-outline";
+            input.type = "password";
+        }
     }
 }
