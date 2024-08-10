70d025b14f19460bba3ba1dbc83775460927bbb8
João Sena <joaosena0206@gmail.com>
2024-08-10 15:40:33 -0300

    Adiciona seletor de data

--- zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
+++ zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cadastro-cliente',
-  templateUrl: './cadastro-cliente.page.html',
-  styleUrls: ['./cadastro-cliente.page.scss'],
+    selector: 'app-cadastro-cliente',
+    templateUrl: './cadastro-cliente.page.html',
+    styleUrls: ['./cadastro-cliente.page.scss'],
 })
-export class CadastroClientePage implements OnInit {
+export class CadastroClientePage implements OnInit {    
+    date: any;
+    teste: any;
+    
+    constructor() {
+    }
 
-  constructor() { }
+    ngOnInit() {
 
-  ngOnInit() {
-  }
-
-  
-
+    }
 }
