8eb4220bd235472650ccf2ef05e2b1af49c3eacc
João Sena <joaosena0206@gmail.com>
2024-08-11 13:44:41 -0300

    Adiciona pesquisa de CEP

--- zelo-paginas/src/app/cliente/endereco/endereco.page.ts
+++ zelo-paginas/src/app/cliente/endereco/endereco.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, viewChild, ViewChild } from '@angular/core';
 
 import { MaskitoOptions, MaskitoElementPredicate } from '@maskito/core';
 
@@ -8,25 +9,40 @@ import { MaskitoOptions, MaskitoElementPredicate } from '@maskito/core';
     styleUrls: ['./endereco.page.scss'],
 })
 export class EnderecoPage implements OnInit {
+    @ViewChild("estado") inputEstado: HTMLIonInputElement;
+    @ViewChild("cidade") inputCidade: HTMLIonInputElement;
+    @ViewChild("bairro") inputBairro: HTMLIonInputElement;
+    @ViewChild("rua") inputRua: HTMLIonInputElement;
+
     readonly cepMask: MaskitoOptions = {
         mask: [/\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/]
     };
 
     readonly maskPredicate: MaskitoElementPredicate = async (el) => (el as HTMLIonInputElement).getInputElement();
     
-    constructor() {
+    constructor(private http: HttpClient) {
         
     }
 
     ngOnInit() {
     }
 
-    filtroInput(event: any, regex: RegExp)
+    buscarCep(cep: any)
     {
-        const input = event.target as HTMLIonInputElement;
-        const vl = input.value;
-        const vlFiltrado = vl?.toString().replace(regex, "");
+        if (cep.length == 9)
+        {
+            const link = `https://viacep.com.br/ws/${cep.replace("-", "")}/json/`;
 
-        event.target.value = vlFiltrado;
+            this.http.get(link).subscribe(res => {
+                let dados: any = res;
+
+                this.inputEstado.value = dados.uf;
+                this.inputCidade.value = dados.localidade;
+                this.inputBairro.value = dados.bairro;
+                this.inputRua.value = dados.logradouro;
+            });
+
+
+        }
     }
 }
