121f2c8bd846705b0b7b5553baca1864149f6745
João Sena <joaosena0206@gmail.com>
2024-08-13 12:48:54 -0300

    Adiciona validação do email

--- zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
+++ zelo-paginas/src/app/cliente/cadastro-cliente/cadastro-cliente.page.ts
@@ -13,19 +13,21 @@ export class CadastroClientePage implements OnInit {
     form = new FormGroup({
         nome: new FormControl("", Validators.required),
         cpf: new FormControl("", [Validators.required, validadorTamanhoMinimo(), validadorCpf()]),
-        data: new FormControl("", [Validators.required, validadorIdade()])
+        data: new FormControl("", [Validators.required, validadorIdade()]),
+        email: new FormControl("", [Validators.required, Validators.email])
     });
-    
+
     inputData: any;
     erro: any = {
         nome: "",
         cpf: "",
-        data: ""
+        data: "",
+        email: ""
     };
     nomeClass: any = "form__input";
 
     erronome: string = "";
-    
+
     regexNome: RegExp = /[^a-zA-Zà-úÀ-úçÇñÑ_ ]+/g;
 
     readonly cpfMask: MaskitoOptions = {
@@ -37,54 +39,46 @@ export class CadastroClientePage implements OnInit {
     };
 
     readonly maskPredicate: MaskitoElementPredicate = async (el) => (el as HTMLIonInputElement).getInputElement();
-    
+
     constructor(private renderer: Renderer2) {
-        
+
     }
 
     ngOnInit() {
     }
 
-    ngAfterViewInit()
-    {
-        
+    ngAfterViewInit() {
+
     }
 
-    estadoSenha(event: any)
-    {
+    estadoSenha(event: any) {
         const olho = event.target as HTMLIonIconElement;
         const input = event.target.parentElement as HTMLIonInputElement;
 
-        if (input.type == "password")
-        {
+        if (input.type == "password") {
             olho.name = "eye-outline";
             input.type = "text";
         }
-        else
-        {
+        else {
             olho.name = "eye-off-outline";
             input.type = "password";
         }
     }
 
-    marcarQuadrado(event: any)
-    {
+    marcarQuadrado(event: any) {
         const quadrado = event.target as HTMLIonIconElement;
 
-        if (quadrado.src == "../../../assets/icon/cliente/quadrado.svg")
-        {
+        if (quadrado.src == "../../../assets/icon/cliente/quadrado.svg") {
             quadrado.setAttribute("marcado", "true");
             quadrado.src = "../../../assets/icon/cliente/quadrado_marcado.svg"
         }
-        else
-        {
+        else {
             quadrado.setAttribute("marcado", "false");
             quadrado.src = "../../../assets/icon/cliente/quadrado.svg"
         }
     }
 
-    filtroInput(event: any, regex: RegExp)
-    {
+    filtroInput(event: any, regex: RegExp) {
         const input = event.target as HTMLIonInputElement;
         const vl = input.value;
         const vlFiltrado = vl?.toString().replace(regex, "");
@@ -92,14 +86,11 @@ export class CadastroClientePage implements OnInit {
         event.target.value = vlFiltrado;
     }
 
-    acharNomeControl(control: FormControl)
-    {
+    acharNomeControl(control: FormControl) {
         let controlName = "";
-        
-        for(let item in this.form.controls)
-        {
-            if(control === this.form.get(item))
-            {
+
+        for (let item in this.form.controls) {
+            if (control === this.form.get(item)) {
                 controlName = item;
             }
         }
@@ -107,34 +98,32 @@ export class CadastroClientePage implements OnInit {
         return controlName;
     }
 
-    validacaoInput(control: FormControl)
-    {
+    validacaoInput(control: FormControl) {
         let nome = this.acharNomeControl(control);
         let vlControl = control.value as String;
 
-        if (control.hasError("required"))
-        {
+        if (control.hasError("required")) {
             this.erro[nome] = `${nome[0].toUpperCase() + nome.replace(nome[0], "")} obrigatório!`;
         }
-        else
+        else if (control.hasError("email"))
         {
+            this.erro[nome] = `Email inválido!`;
+        }
+        else {
             let erros = control.errors;
 
-            if (erros != null)
-            {
+            if (erros != null) {
                 Object.keys(erros).forEach(erro => {
                     this.erro[nome] = erros[erro].msg;
                 });
             }
-        }  
+        }
     }
 
-    mostrarData()
-    {
+    mostrarData() {
         let data = this.form.controls['data'].value;
 
-        if (data != null && data != "")
-        {
+        if (data != null && data != "") {
             let date = new Date(data);
             this.inputData = date.toLocaleDateString();
         }
@@ -143,16 +132,14 @@ export class CadastroClientePage implements OnInit {
 
 
 export function validadorCpf(): ValidatorFn {
-    return (control: AbstractControl) : ValidationErrors | null => {
+    return (control: AbstractControl): ValidationErrors | null => {
         let vl = control.value.replace(/\./g, "").replace("-", "");
 
-        if (vl.length == 11)
-        {
-            if (!cpf.isValid(vl))
-            {
-                return {invalido: {msg: "CPF inválido!"}};
+        if (vl.length == 11) {
+            if (!cpf.isValid(vl)) {
+                return { invalido: { msg: "CPF inválido!" } };
             }
-        
+
             return null;
         }
 
@@ -161,12 +148,11 @@ export function validadorCpf(): ValidatorFn {
 };
 
 export function validadorTamanhoMinimo(): ValidatorFn {
-    return (control: AbstractControl) : ValidationErrors | null => {
+    return (control: AbstractControl): ValidationErrors | null => {
         let vl = control.value.replace(/\./g, "").replace("-", "");
 
-        if (vl.length < 11)
-        {
-            return {tamanhoMinimo: { msg: "CPF deve ter 11 dígitos!"}};
+        if (vl.length < 11) {
+            return { tamanhoMinimo: { msg: "CPF deve ter 11 dígitos!" } };
         }
 
         return null;
@@ -174,20 +160,18 @@ export function validadorTamanhoMinimo(): ValidatorFn {
 };
 
 export function validadorIdade(): ValidatorFn {
-    return (control: AbstractControl) : ValidationErrors | null => {
+    return (control: AbstractControl): ValidationErrors | null => {
         let dataAtual = new Date();
         let dataNascimento = new Date(control.value);
 
         let idade = dataAtual.getFullYear() - dataNascimento.getFullYear();
 
-        if (dataAtual.getDate() < dataNascimento.getDate() && dataAtual.getMonth() + 1 <= dataNascimento.getMonth() + 1)
-        {
+        if (dataAtual.getDate() < dataNascimento.getDate() && dataAtual.getMonth() + 1 <= dataNascimento.getMonth() + 1) {
             idade -= 1;
         }
 
-        if (idade < 18)
-        {
-            return {idade: {msg: "Deve ser maior que 18 anos!"}};
+        if (idade < 18) {
+            return { idade: { msg: "Deve ser maior que 18 anos!" } };
         }
 
         return null;
