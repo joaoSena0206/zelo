0700e1b75e85f9bd6905ecdcfc1121d7c9a8fc9f
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-08-14 12:46:48 -0300

    Adiciona Tela de escolha da categoria

--- zelo-paginas/src/app/app-routing.module.ts
+++ zelo-paginas/src/app/app-routing.module.ts
@@ -18,20 +18,25 @@ const routes: Routes = [
   },
   {
     path: 'cadastro-trabalhador',
-    loadChildren: () => import('./trabalhador/cadastro-trabalhador/cadastro-trabalhador.module').then( m => m.CadastroTrabalhadorPageModule)
+    loadChildren: () => import('./trabalhador/Cadastro/cadastro-trabalhador/cadastro-trabalhador.module').then( m => m.CadastroTrabalhadorPageModule)
   },
   {
     path: 'confirmar-celular',
-    loadChildren: () => import('./trabalhador/confirmar-celular/confirmar-celular.module').then( m => m.ConfirmarCelularPageModule)
+    loadChildren: () => import('./trabalhador/Cadastro/confirmar-celular/confirmar-celular.module').then( m => m.ConfirmarCelularPageModule)
   },
   {
     path: 'tipo-saque',
-    loadChildren: () => import('./trabalhador/tipo-saque/tipo-saque.module').then( m => m.TipoSaquePageModule)
+    loadChildren: () => import('./trabalhador/Cadastro/tipo-saque/tipo-saque.module').then( m => m.TipoSaquePageModule)
   },
   {
     path: 'documento',
-    loadChildren: () => import('./trabalhador/documento/documento.module').then( m => m.DocumentoPageModule)
+    loadChildren: () => import('./trabalhador/Cadastro/documento/documento.module').then( m => m.DocumentoPageModule)
+  },
+  {
+    path: 'categoria',
+    loadChildren: () => import('./trabalhador/Cadastro/categoria/categoria.module').then( m => m.CategoriaPageModule)
   }
+
 
 
 
