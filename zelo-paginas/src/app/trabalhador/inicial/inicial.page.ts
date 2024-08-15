b2cdde711db5e2cc42863b87a80a273fd275047f
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-08-15 18:05:23 -0300

    atualiza a tela inicial com programação

--- zelo-paginas/src/app/trabalhador/inicial/inicial.page.ts
+++ zelo-paginas/src/app/trabalhador/inicial/inicial.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicialPage implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  disponivel()
+  {
+    const botaoSituacao = document.querySelector("#open-custom-dialog");
+    botaoSituacao?.classList.remove("btn_situacao_trabalhador");
+    botaoSituacao?.classList.add('btn_situacao_trabalhador_disponivel');
+
+    const img = document.querySelector(".img_btn_situacao");
+    img?.setAttribute("src", "../../../assets/icon/Trabalhador/IconeAtivo.svg");
+
+    let txtSituacao = document.querySelector(".txtSituacao")?.textContent;
+
   }
 
 }
