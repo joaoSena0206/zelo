a2c915858190a5cb3798c21f5d1922929460ef4c
João Sena <joaosena0206@gmail.com>
2024-08-15 19:30:24 -0300

    Adiciona navegação

--- zelo-paginas/src/app/geral/home/home.page.ts
+++ zelo-paginas/src/app/geral/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-    constructor() { }
+    constructor(private router: Router) { }
+
+    pagEntrar(opcao: string)
+    {
+        localStorage.setItem("opcao", opcao);
+        this.router.navigateByUrl("/home/opcoes-de-cadastro");
+    }
 
 }
--- zelo-paginas/src/app/geral/opcoes-de-cadastro/opcoes-de-cadastro.page.ts
+++ zelo-paginas/src/app/geral/opcoes-de-cadastro/opcoes-de-cadastro.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
-  selector: 'app-opcoes-de-cadastro',
-  templateUrl: './opcoes-de-cadastro.page.html',
-  styleUrls: ['./opcoes-de-cadastro.page.scss'],
+    selector: 'app-opcoes-de-cadastro',
+    templateUrl: './opcoes-de-cadastro.page.html',
+    styleUrls: ['./opcoes-de-cadastro.page.scss'],
 })
 export class OpcoesDeCadastroPage implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router, private navCtrl: NavController) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    proxPag(opcao: string) {
+        this.navCtrl.navigateForward(`/${localStorage.getItem("opcao")}-${opcao}`);
+    }
+
+    pagAnterior() {
+        this.navCtrl.navigateRoot("/home", {
+            animated: true,
+            animationDirection: "back"
+        });
+    }
 }
