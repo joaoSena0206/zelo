ac4afba19f2e48352905f3b1b335fe414b53aa51
MarcoJuino <joaosena0206@gmail.com>
2024-08-16 08:44:31 -0300

    Adiciona pag de recuperar senha

--- zelo-paginas/src/app/app-routing.module.ts
+++ zelo-paginas/src/app/app-routing.module.ts
@@ -35,6 +35,11 @@ const routes: Routes = [
     path: 'confirmar-celular',
     loadChildren: () => import('./cliente/cadastro/confirmar-celular/confirmar-celular.module').then( m => m.ConfirmarCelularPageModule)
   },
+  {
+    path: 'recuperar-senha',
+    loadChildren: () => import('./cliente/login/recuperar-senha/recuperar-senha.module').then( m => m.RecuperarSenhaPageModule)
+  },
+
 ];
 
 @NgModule({
