0e6842b4c1a773c505f07e85003500ee270d037d
João Sena <joaosena0206@gmail.com>
2024-08-17 12:05:20 -0300

    Adiciona header do perfil

--- zelo-paginas/src/app/cliente/logado/perfil/perfil.page.ts
+++ zelo-paginas/src/app/cliente/logado/perfil/perfil.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.page.html',
-  styleUrls: ['./perfil.page.scss'],
+    selector: 'app-perfil',
+    templateUrl: './perfil.page.html',
+    styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    ionViewDidEnter() {
+        const estrelas = document.querySelectorAll(".estrelas ion-icon");
+
+        if (estrelas.length == 3)
+        {
+            (estrelas[1] as HTMLIonIconElement).style.marginBottom = "-40px";
+        }
+        else if (estrelas.length == 4)
+        {
+            (estrelas[1] as HTMLIonIconElement).style.marginBottom = "-40px";
+            (estrelas[2] as HTMLIonIconElement).style.marginBottom = "-40px";
+        }
+        else if (estrelas.length == 5)
+        {
+            (estrelas[0] as HTMLIonIconElement).style.marginTop = "-40px";
+            (estrelas[4] as HTMLIonIconElement).style.marginTop = "-40px";
+            (estrelas[2] as HTMLIonIconElement).style.marginBottom = "-40px";
+        }
+    }
 
 }
