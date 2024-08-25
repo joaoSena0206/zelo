609280a7f1fb7f610e008ab94950be245ec6293f
João Sena <joaosena0206@gmail.com>
2024-08-25 20:57:43 -0300

    Adiciona função para mudar o popup

--- zelo-paginas/src/app/trabalhador/Logado/Servico/trabalhador-caminho/trabalhador-caminho.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/Servico/trabalhador-caminho/trabalhador-caminho.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-trabalhador-caminho',
-  templateUrl: './trabalhador-caminho.page.html',
-  styleUrls: ['./trabalhador-caminho.page.scss'],
+    selector: 'app-trabalhador-caminho',
+    templateUrl: './trabalhador-caminho.page.html',
+    styleUrls: ['./trabalhador-caminho.page.scss'],
 })
 export class TrabalhadorCaminhoPage implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    mudarOutro() {
+        const accordion = document.querySelector(".ion_accordion_group") as HTMLIonAccordionGroupElement;
+        const popup = document.querySelector(".fundoPopup") as HTMLIonTextareaElement;
+
+        accordion.style.display = "none";
+        popup.style.display = "block";
+    }
 }
