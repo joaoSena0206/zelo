ac8fd4597b5bd8f105c14ee28d138f48da6dc72a
João Sena <joaosena0206@gmail.com>
2024-08-27 17:34:27 -0300

    Corrige o não fechamento do modal de aceitar serviço

--- zelo-paginas/src/app/trabalhador/Logado/inicial/inicial.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/inicial/inicial.page.ts
@@ -1,41 +1,41 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-inicial',
-  templateUrl: './inicial.page.html',
-  styleUrls: ['./inicial.page.scss'],
+    selector: 'app-inicial',
+    templateUrl: './inicial.page.html',
+    styleUrls: ['./inicial.page.scss'],
 })
 export class InicialPage implements OnInit {
-  situacao: any = 'Indisponível';
-  msgTrabalho: any = 'Deseja trabalhar agora?';
+    situacao: any = 'Indisponível';
+    msgTrabalho: any = 'Deseja trabalhar agora?';
 
-  constructor() {}
+    constructor() { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
-  disponivel() {
-    const botaoSituacao = document.querySelector('#abrir_modal_servico');
-    const img = document.querySelector('.img_btn_situacao');
+    disponivel() {
+        const botaoSituacao = document.querySelector('#abrir_modal_servico');
+        const img = document.querySelector('.img_btn_situacao');
 
-    if (this.situacao == 'Disponível') {
-      botaoSituacao?.classList.add('btn_situacao_trabalhador');
-      botaoSituacao?.classList.remove('btn_situacao_trabalhador_disponivel');
+        if (this.situacao == 'Disponível') {
+            botaoSituacao?.classList.add('btn_situacao_trabalhador');
+            botaoSituacao?.classList.remove('btn_situacao_trabalhador_disponivel');
 
-      img?.setAttribute('src', '../../../assets/icon/Trabalhador/Icone inicial/IconeOff.svg');
+            img?.setAttribute('src', '../../../assets/icon/Trabalhador/Icone inicial/IconeOff.svg');
 
-      this.situacao = 'Indisponível';
-      this.msgTrabalho = 'Deseja trabalhar agora?';
-    } else {
-      botaoSituacao?.classList.remove('btn_situacao_trabalhador');
-      botaoSituacao?.classList.add('btn_situacao_trabalhador_disponivel');
+            this.situacao = 'Indisponível';
+            this.msgTrabalho = 'Deseja trabalhar agora?';
+        } else {
+            botaoSituacao?.classList.remove('btn_situacao_trabalhador');
+            botaoSituacao?.classList.add('btn_situacao_trabalhador_disponivel');
 
-      img?.setAttribute(
-        'src',
-        '../../../assets/icon/Trabalhador/Icone inicial/IconeAtivo.svg'
-      );
+            img?.setAttribute(
+                'src',
+                '../../../assets/icon/Trabalhador/Icone inicial/IconeAtivo.svg'
+            );
 
-      this.situacao = 'Disponível';
-      this.msgTrabalho = 'Deseja parar de trabalhar?';
+            this.situacao = 'Disponível';
+            this.msgTrabalho = 'Deseja parar de trabalhar?';
+        }
     }
-  }
 }
