c668960a1dbb4ec06bec5548621c74bbbea7bcf3
João Sena <joaosena0206@gmail.com>
2024-09-06 20:30:42 -0300

    Adiciona formatação input

--- zelo-paginas/src/app/cliente/cadastro/confirmar-celular/confirmar-celular.page.ts
+++ zelo-paginas/src/app/cliente/cadastro/confirmar-celular/confirmar-celular.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-confirmar-celular',
-  templateUrl: './confirmar-celular.page.html',
-  styleUrls: ['./confirmar-celular.page.scss'],
+    selector: 'app-confirmar-celular',
+    templateUrl: './confirmar-celular.page.html',
+    styleUrls: ['./confirmar-celular.page.scss'],
 })
 export class ConfirmarCelularPage implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    ngAfterViewInit()
+    {
+        const inputs = document.querySelectorAll("ion-input");
+
+        for (let i = 0; i < inputs.length; i++)
+        {
+            inputs[i].addEventListener("ionInput", function() {
+                let apagado = false;
+                
+                if (/[^\d]/g.test(inputs[i].value?.toString()!))
+                {
+                    apagado = true;
+                }
+
+                inputs[i].value = inputs[i].value?.toString().replace(/[^\d]/g, "");
+
+                if (inputs[i].value != "" && i != inputs.length - 1 && apagado == false)
+                {
+                    inputs[i + 1].setFocus();
+                }
+                else if (inputs[i].value == "" && i != 0 && apagado == false)
+                {
+                    inputs[i - 1].setFocus();
+                }
+            });
+        }
+    }
 }
