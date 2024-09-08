a0cf2a82620f82da5ef8eb53a035c0c3ed3de3f5
João Sena <joaosena0206@gmail.com>
2024-09-08 09:32:34 -0300

    Arruma problemas de alinhamento e espaço

--- zelo-paginas/src/app/cliente/cadastro/confirmar-celular/confirmar-celular.page.ts
+++ zelo-paginas/src/app/cliente/cadastro/confirmar-celular/confirmar-celular.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -13,36 +13,30 @@ export class ConfirmarCelularPage implements OnInit {
     ngOnInit() {
     }
 
-    ngAfterViewInit()
-    {
+    ngAfterViewInit() {
         const inputs = document.querySelectorAll("ion-input");
 
-        for (let i = 0; i < inputs.length; i++)
-        {
-            inputs[i].addEventListener("ionInput", function() {
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener("ionInput", function () {
                 let apagado = false;
-                
-                if (/[^\d]/g.test(inputs[i].value?.toString()!))
-                {
+
+                if (/[^\d]/g.test(inputs[i].value?.toString()!)) {
                     apagado = true;
                 }
 
                 inputs[i].value = inputs[i].value?.toString().replace(/[^\d]/g, "");
 
-                if (inputs[i].value != "" && i != inputs.length - 1 && apagado == false)
-                {
+                if (inputs[i].value != "" && i != inputs.length - 1 && apagado == false) {
                     inputs[i + 1].setFocus();
                 }
-                else if (inputs[i].value == "" && i != 0 && apagado == false)
-                {
+                else if (inputs[i].value == "" && i != 0 && apagado == false) {
                     inputs[i - 1].setFocus();
                 }
             });
         }
     }
 
-    voltarPag()
-    {
+    voltarPag() {
         this.navCl.back();
     }
 }
