b50b0d748e481d562a2062dbe7d5ee079cf3673e
João Sena <joaosena0206@gmail.com>
2024-09-11 18:31:03 -0300

    Remove duplicação de código

--- zelo-paginas/src/app/geral/opcoes-de-cadastro/opcoes-de-cadastro-routing.module.ts
+++ zelo-paginas/src/app/geral/opcoes-de-cadastro/opcoes-de-cadastro-routing.module.ts
@@ -15,21 +15,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class OpcoesDeCadastroPageRoutingModule {}
-
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { OpcoesDeCadastroPage } from './opcoes-de-cadastro.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: OpcoesDeCadastroPage
-  }
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class OpcoesDeCadastroPageRoutingModule {}
