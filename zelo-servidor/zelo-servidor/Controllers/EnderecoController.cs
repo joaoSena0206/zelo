c0ec5998d2313095eb0a81c735abe8149bd389dd
João Sena <joaosena0206@gmail.com>
2024-09-11 16:07:47 -0300

    Adiciona controlador de endereço, junto com o método de adicionar

---
+++ zelo-servidor/zelo-servidor/Controllers/EnderecoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+public class EnderecoController : Controller
+{
+    public void AdicionarEndereco(Endereco endereco)
+    {
+        Banco banco = new Banco();
+        banco.Conectar();
+
+        #region Adiciona o endereço no banco
+
+        string comando = $@"INSERT INTO endereco VALUES 
+        (
+	        (SELECT IFNULL(MAX(cd_endereco) + 1, 1) FROM (SELECT cd_endereco FROM endereco) AS temp),
+	        '{endereco.Cep}',
+	        '{endereco.Identificacao}',
+	        '{endereco.CpfCliente}',
+	        {endereco.Numero},
+	        '{endereco.Complemento}',
+	        '{endereco.Referencia}'
+        );";
+        banco.Executar(comando);
+
+        #endregion 
+    }
+}
