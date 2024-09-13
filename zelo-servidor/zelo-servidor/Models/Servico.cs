d8523b70e8ed58258d0f06031a8fe3180851d23b
João Sena <joaosena0206@gmail.com>
2024-09-13 20:05:53 -0300

    Adiciona modelo serviço

---
+++ zelo-servidor/zelo-servidor/Models/Servico.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Servico
+{
+	private int _codigo;
+
+	public int Codigo
+	{
+		get { return _codigo; }
+		set { _codigo = value; }
+	}
+
+	private int _codigoCategoria;
+
+	public int CodigoCategoria
+	{
+		get { return _codigoCategoria; }
+		set { _codigoCategoria = value; }
+	}
+
+	private string _nome;
+
+	public string Nome
+	{
+		get { return _nome; }
+		set { _nome = value; }
+	}
+
+	public Servico()
+	{
+
+	}
+
+	public Servico(int codigo, int codigoCategoria, string nome)
+	{
+		this.Codigo = codigo;
+		this.CodigoCategoria = codigoCategoria;
+		this.Nome = nome;
+	}
+}
