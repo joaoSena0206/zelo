4399fdc0fb534ccb0399f025d848a7ea9f2e91e4
João Sena <joaosena0206@gmail.com>
2024-09-13 20:01:17 -0300

    Pega as infos do cliente e coloca em uma variável

--- zelo-paginas/src/app/cliente/logado/inicial/inicial.page.ts
+++ zelo-paginas/src/app/cliente/logado/inicial/inicial.page.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-inicial',
@@ -6,11 +7,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
     styleUrls: ['./inicial.page.scss'],
 })
 export class InicialPage implements OnInit {
-    constructor() { }
+    cliente: any = JSON.parse(localStorage.getItem("cliente")!);
+
+    constructor(private http: HttpClient) { }
 
     ngOnInit() {
     }
 
     ngAfterViewInit() {
+        
     }
 }
