d18c53689a93e18001a816a3943254c5c0205c31
João Sena <joaosena0206@gmail.com>
2024-09-13 20:09:00 -0300

    Adiciona modelo da categoria do serviço

---
+++ zelo-servidor/zelo-servidor/Models/CategoriaServico.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class CategoriaServico
+{
+	private int _codigo;
+
+	public int Codigo
+	{
+		get { return _codigo; }
+		set { _codigo = value; }
+	}
+
+	private string _nome;
+
+	public string Nome
+	{
+		get { return _nome; }
+		set { _nome = value; }
+	}
+
+	public CategoriaServico()
+	{
+
+	}
+
+	public CategoriaServico(int codigo, string nome)
+	{
+		this.Codigo = codigo;
+		this.Nome = nome;
+	}
+}
