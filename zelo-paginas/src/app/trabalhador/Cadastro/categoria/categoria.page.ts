9e28a19206ad25f9781623bc041f28bab0890641
João Sena <joaosena0206@gmail.com>
2024-09-20 23:32:20 -0300

    Pega a categoria do localstorage

--- zelo-paginas/src/app/trabalhador/Cadastro/categoria/categoria.page.ts
+++ zelo-paginas/src/app/trabalhador/Cadastro/categoria/categoria.page.ts
@@ -3,68 +3,90 @@ import { noop } from 'rxjs';
 import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-categoria',
-  templateUrl: './categoria.page.html',
-  styleUrls: ['./categoria.page.scss'],
+    selector: 'app-categoria',
+    templateUrl: './categoria.page.html',
+    styleUrls: ['./categoria.page.scss'],
 })
 export class CategoriaPage implements OnInit {
 
-  constructor(private navCl: NavController) {
+    constructor(private navCl: NavController) {
 
-  }
+    }
 
-  ngOnInit() {
-    let listaBotao = document.querySelectorAll('.geralCard ion-card ion-button')
+    voltarPag() {
+        this.navCl.back();
+    }
 
-    for (let i = 0; i < listaBotao.length; i++) {
+    ngAfterViewInit()
+    {
+        if (localStorage.getItem("trabalhador"))
+        {
+            let trabalhador = JSON.parse(localStorage.getItem("trabalhador")!);
 
-      listaBotao[i].addEventListener('click', marcado)
+            const btns = document.querySelectorAll("ion-button");
+            for (let i = 0; i < btns.length; i++)
+            {
+                for (let j = 0; j < trabalhador.categorias.length; j++)
+                {
+                    if (btns[i].textContent == trabalhador.categorias[j])
+                    {
+                        btns[i].setAttribute("id", "marcado");
+                    }
+                }
+            }
+        }
+    }
 
-      function marcado() {
-        let botao = listaBotao[i];
-        let texto = botao.textContent;
+    ngOnInit() {
+        let listaBotao = document.querySelectorAll('.geralCard ion-card ion-button')
 
-        if (botao.id == 'marcado') {
-          botao.id = 'desmarcado';
+        for (let i = 0; i < listaBotao.length; i++) {
+
+            listaBotao[i].addEventListener('click', marcado)
+
+            function marcado() {
+                let botao = listaBotao[i];
+                let texto = botao.textContent;
+
+                if (botao.id == 'marcado') {
+                    botao.id = 'desmarcado';
+                }
+                else {
+                    botao.id = 'marcado';
+                }
+            }
+        }
+    }
+
+
+    enviar() {
+
+        let listaBotao2 = document.querySelectorAll('.geralCard ion-card ion-button')
+        let texto: any;
+        let categorias: any = []
+
+        for (let i = 0; i < listaBotao2.length; i++) {
+
+            if (listaBotao2[i].id == 'marcado') {
+                texto = listaBotao2[i].textContent;
+                categorias.push(texto);
+            }
+
+        }
+
+        if (categorias.length == 0) {
+            console.log('erro')
         }
         else {
-          botao.id = 'marcado';
+            if (localStorage.getItem("trabalhador")) {
+
+                let trabalhadorStorage = JSON.parse(localStorage.getItem("trabalhador")!);
+                trabalhadorStorage.categorias = categorias;
+
+                localStorage.setItem("trabalhador", JSON.stringify(trabalhadorStorage));
+
+                this.navCl.navigateForward("/documento");
+            }
         }
-      }
     }
-  }
-
-
-  enviar() {
-
-    let listaBotao2 = document.querySelectorAll('.geralCard ion-card ion-button')
-    let texto: any;
-    let categorias: any = []
-
-    for (let i = 0; i < listaBotao2.length; i++) {
-
-      if (listaBotao2[i].id == 'marcado') {
-        texto = listaBotao2[i].textContent;
-        categorias.push(texto);
-      }
-
-    }
-
-    if (categorias.length == 0) 
-    {
-      console.log('erro')
-    }
-    else 
-    {
-      if (localStorage.getItem("trabalhador")) {
-
-        let trabalhadorStorage = JSON.parse(localStorage.getItem("trabalhador")!);
-        trabalhadorStorage.categorias = categorias;
-
-        localStorage.setItem("trabalhador", JSON.stringify(trabalhadorStorage));
-
-        this.navCl.navigateForward("/documento");
-      }
-    }
-  }
 }
