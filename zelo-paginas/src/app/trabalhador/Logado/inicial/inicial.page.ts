0e16676a9a7c7194ede19b8b2f0f5df38cf87cf1
MarcoJuino <marcojuino.07@gmail.com>
2024-09-26 10:54:24 -0300

    atualiza a tela  inicial

--- zelo-paginas/src/app/trabalhador/Logado/inicial/inicial.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/inicial/inicial.page.ts
@@ -8,6 +8,65 @@ import { HttpClient } from '@angular/common/http';
 })
 export class InicialPage implements OnInit {
 
+    /* trabalhador: any = JSON.parse(localStorage.getItem("trabalhador")!);
+    categorias: any;
+    patrocinados: any;
+    historico: any;
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        this.carregarCategorias();
+        this.carregarHistorico();
+    }
+
+    carregarHistorico() {
+        let trabalhador = JSON.parse(localStorage.getItem("trabalhador")!);
+        let link = `http://localhost:57879/SolicitacaoServico/CarregarUltimosPedidos?c=${trabalhador.Cpf}&t=cliente`;
+
+        this.http.get(link).subscribe(res => {
+            this.historico = res;
+            console.log(this.historico);
+        });
+    }
+
+    carregarCategorias() {
+
+        let link = "http://localhost:57879/CategoriaServico/CarregarCategoria";
+
+        this.http.get(link).subscribe(res => {
+            this.categorias = res;
+
+            if (this.categorias != null) {
+                link = "http://localhost:57879/Servico/CarregarServicos";
+
+                this.http.get(link).subscribe(res2 => {
+                    
+                    let servicos: any = res2;
+
+                    if (servicos != null) {
+                        for (let i = 0; i < this.categorias.length; i++) {
+                            this.categorias[i].Servicos = [];
+
+                            for (let j = 0; j < servicos.length; j++) {
+                                if (j == 5) {
+                                    break;
+                                }
+
+                                if (this.categorias[i].Codigo == servicos[j].CodigoCategoria) {
+                                    this.categorias[i].Servicos.push(servicos[j]);
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    } */
+
     msgTrabalho: any = 'Deseja trabalhar agora?';
 
     constructor(private http: HttpClient) { }
@@ -66,9 +125,6 @@ export class InicialPage implements OnInit {
                                                 <p class="ds_pedido">${lista[i]['DsServico']}</p>`;
 
                 areaUltimosPedidos?.appendChild(divUltimosPedidos);
-
-                
-
             }
             
         });
