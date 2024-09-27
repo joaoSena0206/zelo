d21a1a1d50b7ec7431ee0a209009f16a558a3a20
João Sena <joaosena0206@gmail.com>
2024-09-26 21:06:51 -0300

    Arruma o erro de não mudar o css quando o Angular renderizar

--- zelo-paginas/src/app/cliente/logado/trabalhos/trabalhos.page.ts
+++ zelo-paginas/src/app/cliente/logado/trabalhos/trabalhos.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-trabalhos',
@@ -6,22 +9,24 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./trabalhos.page.scss'],
 })
 export class TrabalhosPage implements OnInit {
-    constructor() { }
+    cliente: any = JSON.parse(localStorage.getItem("cliente")!);
+    carregar: boolean = false;
+    categorias: any;
+
+    @ViewChildren("ngContainer") ngContainer: QueryList<ElementRef>;
+
+    constructor(private http: HttpClient) { }
 
     ngOnInit() {
 
     }
 
     ionViewDidEnter() {
-        const cards = document.querySelectorAll(".local_ultimos_trabalhos ion-card");
+        this.carregarCategorias();
+    }
 
-        for (let i = 0; i < cards.length; i++) {
-            if ((cards[i] as HTMLIonCardElement).clientWidth != (cards[0] as HTMLIonCardElement).clientWidth) {
-                (cards[i] as HTMLIonCardElement).style.maxWidth = `${(cards[0] as HTMLIonCardElement).clientWidth}px`;
-            }
-        }
-
-        window.addEventListener("resize", function () {
+    ngAfterViewInit() {
+        this.ngContainer.changes.subscribe(() => {
             const cards = document.querySelectorAll(".local_ultimos_trabalhos ion-card");
 
             for (let i = 0; i < cards.length; i++) {
@@ -29,6 +34,56 @@ export class TrabalhosPage implements OnInit {
                     (cards[i] as HTMLIonCardElement).style.maxWidth = `${(cards[0] as HTMLIonCardElement).clientWidth}px`;
                 }
             }
+
+            window.addEventListener("resize", function () {
+                const cards = document.querySelectorAll(".local_ultimos_trabalhos ion-card");
+
+                for (let i = 0; i < cards.length; i++) {
+                    if ((cards[i] as HTMLIonCardElement).clientWidth != (cards[0] as HTMLIonCardElement).clientWidth) {
+                        (cards[i] as HTMLIonCardElement).style.maxWidth = `${(cards[0] as HTMLIonCardElement).clientWidth}px`;
+                    }
+                }
+            });
         });
+    }
+
+    async carregarCategorias() {
+        let link = "http://localhost:57879/CategoriaServico/CarregarCategoria";
+
+        this.carregar = true;
+
+        let res = await firstValueFrom(this.http.get(link));
+        this.categorias = res;
+
+        if (this.categorias != null) {
+            link = "http://localhost:57879/Servico/CarregarServicos";
+
+            let res2 = await firstValueFrom(this.http.get(link));
+
+            let servicos: any = res2;
+            servicos.sort((a: any, b: any) => {
+                if (a.Nome < b.Nome) {
+                    return -1;
+                }
+                if (a.Nome > b.Nome) {
+                    return 1;
+                }
+                return 0;
+            });
+
+            if (servicos != null) {
+                for (let i = 0; i < this.categorias.length; i++) {
+                    this.categorias[i].Servicos = [];
+
+                    for (let j = 0; j < servicos.length; j++) {
+                        if (this.categorias[i].Codigo == servicos[j].CodigoCategoria) {
+                            this.categorias[i].Servicos.push(servicos[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        this.carregar = false;
     }
 }
