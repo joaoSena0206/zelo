41d946ea8074fa04e011447007b501154b3b4f1c
João Sena <joaosena0206@gmail.com>
2024-09-29 11:39:35 -0300

    Mostra os trabalhadores pelo ngFor

--- zelo-paginas/src/app/cliente/logado/servico/escolher-trabalhador/escolher-trabalhador.page.ts
+++ zelo-paginas/src/app/cliente/logado/servico/escolher-trabalhador/escolher-trabalhador.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-escolher-trabalhador',
-  templateUrl: './escolher-trabalhador.page.html',
-  styleUrls: ['./escolher-trabalhador.page.scss'],
+    selector: 'app-escolher-trabalhador',
+    templateUrl: './escolher-trabalhador.page.html',
+    styleUrls: ['./escolher-trabalhador.page.scss'],
 })
 export class EscolherTrabalhadorPage implements OnInit {
+    carregar: boolean = false;
+    trabalhadores: any;
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        let servico = JSON.parse(localStorage.getItem("servico")!);
 
+        this.carregarTrabalhadores(servico.Codigo);
+    }
+
+    ionViewDidEnter() {
+
+    }
+
+    async carregarTrabalhadores(codigo: number) {
+        let link = "http://localhost:57879/Trabalhador/CarregarTrabalhadores?c=" + codigo;
+
+        this.carregar = true;
+        let resposta = await firstValueFrom(this.http.get(link));
+        this.carregar = false;
+
+        this.trabalhadores = resposta;
+    }
 }
