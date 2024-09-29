ff50e40c88b926c27e58fe922a61ddf034e94bf0
João Sena <joaosena0206@gmail.com>
2024-09-29 11:40:12 -0300

    Adiciona modelo serviço_trabalhador

---
+++ zelo-servidor/zelo-servidor/Models/ServicoTrabalhador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class ServicoTrabalhador
+{
+	private Trabalhador _trabalhador;
+
+	public Trabalhador Trabalhador
+	{
+		get { return _trabalhador; }
+		set { _trabalhador = value; }
+	}
+
+	private Servico _servico;
+
+	public Servico Servico
+	{
+		get { return _servico; }
+		set { _servico = value; }
+	}
+
+	public ServicoTrabalhador()
+	{
+
+	}
+
+    public ServicoTrabalhador(Trabalhador trabalhador, Servico servico)
+    {
+        Trabalhador = trabalhador;
+        Servico = servico;
+    }
+}
