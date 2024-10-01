b883146f4cfbeeb04ff0a269ec3e8306efc91ed3
João Sena <joaosena0206@gmail.com>
2024-10-01 11:38:31 -0300

    Checa se o usuário está logado

--- zelo-paginas/src/app/geral/home/home.page.ts
+++ zelo-paginas/src/app/geral/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
     selector: 'app-home',
@@ -8,10 +9,20 @@ import { Route, Router } from '@angular/router';
 })
 export class HomePage {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private navCl: NavController) { }
 
-    pagEntrar(opcao: string)
-    {
+    ngAfterViewInit() {
+        if (localStorage.getItem("logado") == "true") {
+            if (localStorage.getItem("trabalhador")) {
+                this.navCl.navigateRoot("trabalhador/inicial");
+            }
+            else {
+                this.navCl.navigateRoot("inicial");
+            }
+        }
+    }
+
+    pagEntrar(opcao: string) {
         localStorage.setItem("opcao", opcao);
         this.router.navigateByUrl("/home/opcoes-de-cadastro");
     }
