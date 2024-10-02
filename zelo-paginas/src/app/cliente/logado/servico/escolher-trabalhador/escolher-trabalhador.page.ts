f587a530f645e0a79f589a8a04e9fc4a93be8758
João Sena <joaosena0206@gmail.com>
2024-10-02 15:26:11 -0300

    Adiciona voltar pag

--- zelo-paginas/src/app/cliente/logado/servico/escolher-trabalhador/escolher-trabalhador.page.ts
+++ zelo-paginas/src/app/cliente/logado/servico/escolher-trabalhador/escolher-trabalhador.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { headerNgrok } from 'src/app/gerais';
+import { NavController } from '@ionic/angular';
 
 @Component({
     selector: 'app-escolher-trabalhador',
@@ -11,7 +13,7 @@ export class EscolherTrabalhadorPage implements OnInit {
     carregar: boolean = false;
     trabalhadores: any;
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private navCl: NavController) { }
 
     ngOnInit() {
         let servico = JSON.parse(localStorage.getItem("servico")!);
@@ -23,11 +25,16 @@ export class EscolherTrabalhadorPage implements OnInit {
 
     }
 
+    voltarPag()
+    {
+        this.navCl.back();
+    }
+
     async carregarTrabalhadores(codigo: number) {
         let link = "https://chow-master-properly.ngrok-free.app/Trabalhador/CarregarTrabalhadores?c=" + codigo;
 
         this.carregar = true;
-        let resposta = await firstValueFrom(this.http.get(link));
+        let resposta = await firstValueFrom(this.http.get(link, {headers: headerNgrok}));
         this.carregar = false;
 
         this.trabalhadores = resposta;
