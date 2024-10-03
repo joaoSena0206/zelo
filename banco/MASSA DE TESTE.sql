b142b20aa431a500bca4ac73f1611cde60abc737
João Sena <joaosena0206@gmail.com>
2024-10-03 14:06:08 -0300

    Adiciona serviço ao Marco

--- banco/MASSA DE TESTE.sql
+++ banco/MASSA DE TESTE.sql
@@ -3,7 +3,7 @@ USE Zelo;
 INSERT INTO trabalhador VALUE('56787654567', 'Robson Santos', '2006-05-24', '2020-05-20', 'robsonsantos@gmail.com', md5('123'), 'dasdads', true, true, 25.50, -23.9560, -46.3305);
 INSERT INTO trabalhador VALUE('56787654566', 'Julberto', '2006-05-24', '2021-05-20', 'julberto@gmail.com', md5('123'), 'dasdads', true, true, 32.65, -23.9336, -46.3221);
 Insert into trabalhador value('50082480818', 'João Sena', '2006-06-24', '2024-09-20', 'joaosena0206@gmail.com', md5('1234'), '50082480818', false, true, 25, -23.9738, -46.3437);
-Insert into trabalhador value('53890618880', 'Marco', '2006-06-24', '2024-09-20', 'marcojuino.07@gmail.com', md5('1234'), '50082480818', false, true, 25, -23.9524, -46.3603);
+Insert into trabalhador value('53890618880', 'Marco', '2006-06-24', '2024-09-20', 'marcojuino.07@gmail.com', md5('1234'), '50082480818', true, true, 25, -23.9524, -46.3603);
 
 INSERT INTO patrocinio VALUE(
 	'56787654567', 2, 2, 200, '2024-09-21'
@@ -14,6 +14,7 @@ INSERT INTO patrocinio VALUE(
 	'56787654566', 7, 1, 100, '2024-09-20'
 );
 
+Insert into servico_trabalhador values('53890618880', 3);
 Insert into servico_trabalhador values('56787654567', 2);
 Insert into servico_trabalhador values('56787654566', 7);
 Insert into servico_trabalhador values('50082480818', 3);
