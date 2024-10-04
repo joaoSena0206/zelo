130b8a3427a7e1395562b27dc05283603c760896
João Sena <joaosena0206@gmail.com>
2024-10-04 20:09:20 -0300

    Muda o id do app

--- zelo-paginas/capacitor.config.ts
+++ zelo-paginas/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import { interval } from 'rxjs';
 
 const config: CapacitorConfig = {
-    appId: 'com.zelo.zeloapp',
+    appId: 'com.zelo.app',
     appName: 'zelo-paginas',
     webDir: 'www',
     android: {
