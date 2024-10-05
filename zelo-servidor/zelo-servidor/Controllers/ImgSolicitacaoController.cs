c6e6667b42c8c0fb97e5ab97f5cbce45e0dbb3ae
João Sena <joaosena0206@gmail.com>
2024-10-05 12:43:14 -0300

    Adiciona método de carregar e inserir imgs

---
+++ zelo-servidor/zelo-servidor/Controllers/ImgSolicitacaoController.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+[RoutePrefix("ImgSolicitacao")]
+public class ImgSolicitacaoController : Controller
+{
+    public void AdicionarImgs(int codigoImg, int codigoSolicitacao, string tipoArquivo)
+    {
+        Banco banco = new Banco();
+        banco.Conectar();
+
+        #region Insere as imgs no banco
+
+        string comando = $@"INSERT INTO img_solicitacao VALUES
+        (
+	        {codigoImg},
+	        {codigoSolicitacao},
+	        '{tipoArquivo}'
+        )";
+        banco.Executar(comando);
+        banco.Desconectar();
+
+        #endregion
+    }
+
+    [HttpGet]
+    [Route("CarregarImgs")]
+    public string CarregarImgs(int cdSolicitacao)
+    {
+        Banco banco = new Banco();
+        banco.Conectar();
+
+        #region Carrega as imgs do banco e envia
+
+        string comando = $"SELECT * FROM img_solicitacao";
+        MySqlDataReader dados = banco.Consultar(comando);
+
+        List<ImgSolicitacao> lista = new List<ImgSolicitacao>();
+
+        if (dados != null)
+        {
+            while (dados.Read())
+            {
+                ImgSolicitacao img = new ImgSolicitacao();
+                SolicitacaoServico solicitacao = new SolicitacaoServico();
+
+                solicitacao.CdSolicitacaoServico = dados.GetInt32(1);
+
+                img.Codigo = dados.GetInt32(0);
+                img.TipoArquivo = dados.GetString(2);
+                img.Solicitacao = solicitacao;
+
+                lista.Add(img);
+            }
+        }
+
+        dados.Close();
+        banco.Desconectar();
+
+        return JsonConvert.SerializeObject(lista, Formatting.Indented);
+
+        #endregion
+    }
+}
