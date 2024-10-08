6f16557547e3705431411854502df4ac368baca8
João Sena <joaosena0206@gmail.com>
2024-10-07 22:51:11 -0300

    Muda dominio

--- zelo-paginas/src/app/gerais.ts
+++ zelo-paginas/src/app/gerais.ts
@@ -4,4 +4,4 @@ export const headerNgrok = new HttpHeaders({
     "ngrok-skip-browser-warning": "true"
 });
 
-export const dominio = "http://zelo-server.com.br";
+export const dominio = "https://comparisons-adjustable-commodity-loaded.trycloudflare.com";
