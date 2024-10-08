059159db76bb30ac90dbc262a2707bb634c630de
João Sena <joaosena0206@gmail.com>
2024-10-08 00:39:27 -0300

    Converte o blob em arquivo

--- zelo-paginas/src/app/cliente/logado/servico/descricao-servico/descricao-servico.page.ts
+++ zelo-paginas/src/app/cliente/logado/servico/descricao-servico/descricao-servico.page.ts
@@ -146,10 +146,11 @@ export class DescricaoServicoPage implements OnInit {
 
         let resposta = await fetch(imgArquivo.webPath!);
         let blob = await resposta.blob();
+        let file = new File([blob], "img." + blob.type.substring(blob.type.indexOf("/") + 1), { type: blob.type });
 
         let arquivo = {
             src: imgArquivo.webPath,
-            blob: blob
+            blob: file
         };
 
         this.imgs.push(arquivo);
