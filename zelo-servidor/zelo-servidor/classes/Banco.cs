dbc42723b1e59babef056db9f52e35bcfb68e789
João Sena <joaosena0206@gmail.com>
2024-10-16 19:42:37 -0300

    Altera server mysql

--- zelo-servidor/zelo-servidor/classes/Banco.cs
+++ zelo-servidor/zelo-servidor/classes/Banco.cs
@@ -14,7 +14,7 @@ public class Banco
     {
         if (conexao == null)
         {
-            string linhaConexao = "SERVER=localhost; UID=root; PASSWORD=root; DATABASE=zelo";
+            string linhaConexao = "SERVER=autorack.proxy.rlwy.net; PORT=41395; UID=root; PASSWORD=root; DATABASE=Zelo";
             conexao = new MySqlConnection(linhaConexao);
 
             conexao.Open();
