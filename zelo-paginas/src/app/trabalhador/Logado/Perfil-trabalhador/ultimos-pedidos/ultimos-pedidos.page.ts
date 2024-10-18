8856871652e4d6157f9204bc2aba8a6f6709b156
BrenoF013 <148352161+BrenoF013@users.noreply.github.com>
2024-10-18 11:37:57 -0300

    AdicionaHistoricoTrabalhador

--- zelo-paginas/src/app/trabalhador/Logado/Perfil-trabalhador/ultimos-pedidos/ultimos-pedidos.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/Perfil-trabalhador/ultimos-pedidos/ultimos-pedidos.page.ts
@@ -1,5 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IonList, ModalController } from '@ionic/angular';
+import { Geolocation } from '@capacitor/geolocation';
+import { first, firstValueFrom } from 'rxjs';
+import { BackgroundRunner } from '@capacitor/background-runner';
+import { dominio, headerNgrok } from 'src/app/gerais';
 import { NavController } from '@ionic/angular';
+import { BackgroundGeolocationPlugin } from "@capacitor-community/background-geolocation";
+import { registerPlugin } from '@capacitor/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { DOCUMENT } from '@angular/common';
+import {
+    ActionPerformed,
+    PushNotificationSchema,
+    PushNotifications,
+    Token,
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-ultimos-pedidos',
@@ -8,13 +24,61 @@ import { NavController } from '@ionic/angular';
 })
 export class UltimosPedidosPage implements OnInit {
 
-  constructor(private navCl: NavController) { }
+  constructor(private http: HttpClient, private navCl: NavController) { }
 
   ngOnInit() {
   }
 
   voltarPag() {
       this.navCl.back();
+  }
+
+    trabalhador: any = JSON.parse(localStorage.getItem("trabalhador")!);
+    historico: any;
+    Nome: any = this.trabalhador.Nome.trim();
+    ComentarioAnonimo: any;
+    qtEstrelas: any;
+    carregar: boolean = false;
+    watcherId: any;
+    result: any;
+    clienteServico: any;
+    solicitacaoServico: any;
+    enderecoServico: any;
+    modal: any;
+
+  async carregarHistorico() {
+    let trabalhador = JSON.parse(localStorage.getItem("trabalhador")!);
+    let link = dominio + `/SolicitacaoServico/CarregarUltimosPedidos?c=${trabalhador.Cpf}&t=trabalhador`;
+
+    this.carregar = true;
+    let res: any = await firstValueFrom(this.http.get(link, { headers: headerNgrok }));
+
+    for (let i = 0; i < res.length; i++) {
+        res[i].img = await this.carregarImgServico(res[i].CdSolicitacaoServico);
+    }
+
+    this.historico = res;
+}
+
+  async carregarImgServico(cdSolicitacao: any) {
+      this.carregar = true;
+
+      let link = dominio + `/ImgSolicitacao/CarregarImgs?c=${cdSolicitacao}&q=1`;
+      let res: any = await firstValueFrom(this.http.get(link, { headers: headerNgrok }));
+
+      if (res.length > 0) {
+          link = dominio + `/Imgs/Solicitacao/${cdSolicitacao}/1${res[0].TipoArquivo}`;
+      }
+      else {
+          link = "../../../../assets/icon/geral/sem-foto.jpg";
+      }
+
+      res = await firstValueFrom(this.http.get(link, { headers: headerNgrok, responseType: "blob" }));
+
+      const urlImg = URL.createObjectURL(res);
+      this.carregar = false;
+
+      return urlImg;
   }
 
 }
