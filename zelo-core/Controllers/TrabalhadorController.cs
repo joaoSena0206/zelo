9d62eea34118420ba561df3d12c2bc721ba29760
João Sena <joaosena0206@gmail.com>
2024-10-23 19:55:15 -0300

    Muda pra post o método

--- zelo-core/Controllers/TrabalhadorController.cs
+++ zelo-core/Controllers/TrabalhadorController.cs
@@ -38,8 +38,8 @@ public class TrabalhadorController : ControllerBase
         }
     }
 
-    [HttpGet("AdicionarFotoPerfil")]
-    public async Task<IActionResult> AdicionarFotoPerfil([FromQuery] string cpf, [FromQuery] IFormFile? file = null)
+    [HttpPost("AdicionarFotoPerfil")]
+    public async Task<IActionResult> AdicionarFotoPerfil([FromForm] string cpf, [FromForm] IFormFile? file = null)
     {
         try
         {
