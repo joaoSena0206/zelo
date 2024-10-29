b50bfe77fa3278314deec75142efe215bb3094aa
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-10-29 18:19:43 -0300

    atualiza tela de privacidade

--- zelo-paginas/src/app/trabalhador/Logado/Perfil-trabalhador/privacidade/privacidade.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/Perfil-trabalhador/privacidade/privacidade.page.ts
@@ -20,8 +20,30 @@ export class PrivacidadePage implements OnInit {
   }
 
   pegarDados() {
-    const indicePonto = this.trabalhador.DataNascimento.indexOf("T");
-    this.novoTexto = this.trabalhador.DataNascimento.substring(0, indicePonto);
+    const dateString: string = this.trabalhador.dataNascimento;
+      const timestamp: number = Date.parse(dateString);
+      const date: Date = new Date(timestamp);
+
+      let dia = date.getDate();
+      let mes = date.getMonth() + 1;
+      let ano = date.getFullYear();
+
+      let DataFormatada = null;
+
+      if(mes < 10)
+      {
+        DataFormatada = dia + "/" + "0" + mes + "/" + ano;
+      }
+      else
+      {
+        DataFormatada = dia + "/" + mes + "/" + ano;
+      }
+
+      this.novoTexto = DataFormatada;
+  }
+
+  editar(btn: any){
+    console.log(btn);
   }
 
 }
