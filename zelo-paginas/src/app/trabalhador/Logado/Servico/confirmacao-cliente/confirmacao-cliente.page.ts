2c2e93dc405302b5a78d453bd6839d9c37ee7b2d
João Sena <joaosena0206@gmail.com>
2024-11-16 14:31:13 -0300

    Adiciona cancelamento

--- zelo-paginas/src/app/trabalhador/Logado/Servico/confirmacao-cliente/confirmacao-cliente.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/Servico/confirmacao-cliente/confirmacao-cliente.page.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  ActionPerformed,
-  PushNotificationSchema,
-  PushNotifications,
-  Token,
+    ActionPerformed,
+    PushNotificationSchema,
+    PushNotifications,
+    Token,
 } from '@capacitor/push-notifications';
 import { NavController } from '@ionic/angular';
 import { dominio, headerNgrok } from 'src/app/gerais';
@@ -11,84 +11,83 @@ import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
 @Component({
-  selector: 'app-confirmacao-cliente',
-  templateUrl: './confirmacao-cliente.page.html',
-  styleUrls: ['./confirmacao-cliente.page.scss'],
+    selector: 'app-confirmacao-cliente',
+    templateUrl: './confirmacao-cliente.page.html',
+    styleUrls: ['./confirmacao-cliente.page.scss'],
 })
 export class ConfirmacaoClientePage implements OnInit {
 
-  tempo: any;
-  constructor(private navCl: NavController, private http: HttpClient) { }
+    tempo: any;
+    constructor(private navCl: NavController, private http: HttpClient) { }
 
-  ngOnInit() {
-    PushNotifications.addListener("pushNotificationReceived", (notification: PushNotificationSchema) => {
+    ngOnInit() {
+        PushNotifications.addListener("pushNotificationReceived", (notification: PushNotificationSchema) => {
 
-      let situacao = notification.data.pago;
+            let situacao = notification.data.pago;
 
-      if (situacao == "true") {
-        this.navCl.navigateRoot("trabalhador/trabalhador-caminho");
-      }
-      else {
-        this.cancelar();
-      }
+            if (situacao == "true") {
+                this.navCl.navigateRoot("trabalhador/trabalhador-caminho");
+            }
+            else {
+                this.cancelar();
+            }
 
-    });
+        });
 
-    if (!localStorage.getItem("confirmacao")) {
-      let confirmacao = {
-        min: 10,
-        seg: 0
-      };
+        if (!localStorage.getItem("confirmacao")) {
+            let confirmacao = {
+                min: 10,
+                seg: 0
+            };
 
-      this.tempo = confirmacao;
-      localStorage.setItem("confirmacao", JSON.stringify(confirmacao));
-    }
-    else {
-      this.tempo = JSON.parse(localStorage.getItem("confirmacao")!);
-    }
+            this.tempo = confirmacao;
+            localStorage.setItem("confirmacao", JSON.stringify(confirmacao));
+        }
+        else {
+            this.tempo = JSON.parse(localStorage.getItem("confirmacao")!);
+        }
 
-    this.temporizador();
-  }
-
-  temporizador() {
-    if (this.tempo.min.toString().length == 1) {
-      this.tempo.min = "0" + this.tempo.min.toString();
+        this.temporizador();
     }
 
-    if (this.tempo.seg.toString().length == 1) {
-      this.tempo.seg = "0" + this.tempo.seg.toString();
+    temporizador() {
+        if (this.tempo.min.toString().length == 1) {
+            this.tempo.min = "0" + this.tempo.min.toString();
+        }
+
+        if (this.tempo.seg.toString().length == 1) {
+            this.tempo.seg = "0" + this.tempo.seg.toString();
+        }
+
+        let id = setInterval(() => {
+            if (Number(this.tempo.seg) == 0) {
+                this.tempo.seg = 60;
+                this.tempo.min -= 1;
+            }
+
+            if (this.tempo.min.toString().length == 1) {
+                this.tempo.min = "0" + this.tempo.min.toString();
+            }
+
+            this.tempo.seg -= 1;
+
+            if (this.tempo.seg.toString().length == 1) {
+                this.tempo.seg = "0" + this.tempo.seg.toString();
+            }
+
+            localStorage.setItem("confirmacao", JSON.stringify(this.tempo));
+
+            if (Number(this.tempo.min) == 0 && Number(this.tempo.seg) == 0) {
+                this.cancelar();
+
+                clearInterval(id);
+            }
+        }, 1000);
     }
 
-    let id = setInterval(() => {
-      if (Number(this.tempo.seg) == 0) {
-        this.tempo.seg = 60;
-        this.tempo.min -= 1;
-      }
-
-      if (this.tempo.min.toString().length == 1) {
-        this.tempo.min = "0" + this.tempo.min.toString();
-      }
-
-      this.tempo.seg -= 1;
-
-      if (this.tempo.seg.toString().length == 1) {
-        this.tempo.seg = "0" + this.tempo.seg.toString();
-      }
-
-      localStorage.setItem("confirmacao", JSON.stringify(this.tempo));
-
-      if (Number(this.tempo.min) == 0 && Number(this.tempo.seg) == 0) {
-        this.cancelar();
-
-        clearInterval(id);
-      }
-    }, 1000);
-  }
-
-  cancelar() {
-    localStorage.removeItem("confirmacao");
-    this.navCl.navigateBack("/trabalhador/inicial");
-    console.log(this.tempo);
-  }
+    cancelar() {
+        localStorage.removeItem("confirmacao");
+        this.navCl.navigateBack("/trabalhador/inicial");
+    }
 
 }
