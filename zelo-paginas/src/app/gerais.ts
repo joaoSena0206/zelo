5ad7bf8b662f71cf3e4e23068c54abd9047ea60d
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-11-18 23:22:42 -0300

    muda dominio

--- zelo-paginas/src/app/gerais.ts
+++ zelo-paginas/src/app/gerais.ts
@@ -4,5 +4,5 @@ export const headerNgrok = new HttpHeaders({
     "ngrok-skip-browser-warning": "true"
 });
 
-export const dominio = "https://absent-truly-resumes-media.trycloudflare.com";
+export const dominio = "https://arena-instructions-river-thinks.trycloudflare.com";
 export const apiGoogle = "AIzaSyDLQuCu8-clWnemW9ey9s5Hpz2vulxMEzM";
