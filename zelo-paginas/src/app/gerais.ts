a250aebc7baf3b542266509df0fe55c755231486
João Sena <joaosena0206@gmail.com>
2024-11-20 02:00:56 -0300

    Muda o dominio

--- zelo-paginas/src/app/gerais.ts
+++ zelo-paginas/src/app/gerais.ts
@@ -1,8 +1,18 @@
 import { HttpHeaders } from "@angular/common/http";
+import { FirebaseOptions } from "firebase/app";
 
 export const headerNgrok = new HttpHeaders({
     "ngrok-skip-browser-warning": "true"
 });
 
-export const dominio = "https://arena-instructions-river-thinks.trycloudflare.com";
+export const dominio = "https://zelo-server.com.br";
 export const apiGoogle = "AIzaSyDLQuCu8-clWnemW9ey9s5Hpz2vulxMEzM";
+export const ambient_firebase = {
+    apiKey: "AIzaSyBYK20Z_BR4TnpM27C1Hm-M-aI7o51oEIg",
+    authDomain: "zelo-dfe99.firebaseapp.com",
+    projectId: "zelo-dfe99",
+    storageBucket: "zelo-dfe99.firebasestorage.app",
+    messagingSenderId: "335729234108",
+    appId: "1:335729234108:web:fb5cfd26867fdd813440b9",
+    measurementId: "G-MHTY1LJZQT"
+};
