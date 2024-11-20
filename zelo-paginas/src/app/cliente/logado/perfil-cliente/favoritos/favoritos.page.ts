0e64cb3c40fe1a53a92183129a97fa1a33beafd3
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-11-20 01:41:43 -0300

    adiciona favoritos ts

--- zelo-paginas/src/app/cliente/logado/perfil-cliente/favoritos/favoritos.page.ts
+++ zelo-paginas/src/app/cliente/logado/perfil-cliente/favoritos/favoritos.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { dominio, headerNgrok } from 'src/app/gerais';
+import { NavController } from '@ionic/angular';
+import { first, firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-favoritos',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritosPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private navCl: NavController) { }
 
   ngOnInit() {
+  }
+
+  voltarPag() {
+    this.navCl.back();
+  }
+
+  cliente: any = JSON.parse(localStorage.getItem("cliente")!);
+  historico: any;
+  Nome: any = this.cliente.Nome;
+  ComentarioAnonimo: any;
+  qtEstrelas: any;
+  carregar: boolean = false;
+  watcherId: any;
+  result: any;
+  clienteServico: any;
+  solicitacaoServico: any;
+  enderecoServico: any;
+  modal: any;
+  listaDataServico: any = [];
+  dominio = dominio;
+
+  async carregarHistorico() {
+    let link = dominio + `/SolicitacaoServico/CarregarHistoricoCliente?cpf=${this.cliente.Cpf}&tipo=favoritos`;
+
+    try {
+
+      this.carregar = true;
+      let res: any = await firstValueFrom(this.http.get(link, { headers: headerNgrok }));
+
+      this.historico = res;
+
+    }
+    catch (erro: any) {
+      const alert = document.querySelector("ion-alert") as HTMLIonAlertElement;
+      alert.message = "Erro ao conectar-se ao servidor";
+      alert.present();
+    }
+    finally {
+      this.carregar = false;
+    }
+      
+  }
+    
+  VerPerfil(Cpf: any, Nome: any){
+    let clienteVerPerfil = {Cpf: Cpf, Nome: Nome}
+    localStorage.setItem('perfil', JSON.stringify(clienteVerPerfil))
+    this.navCl.navigateBack("/perfil-trabalhador")
+  }
+
+  ngAfterViewInit() {
+    this.carregarHistorico();
   }
 
 }
