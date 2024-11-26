0928ed24ece8d3de519642de6b441530967862ad
João Sena <joaosena0206@gmail.com>
2024-11-26 18:40:23 -0300

    Dominio

--- zelo-paginas/src/app/gerais.ts
+++ zelo-paginas/src/app/gerais.ts
@@ -5,7 +5,7 @@ export const headerNgrok = new HttpHeaders({
     "ngrok-skip-browser-warning": "true"
 });
 
-export const dominio = "https://punch-monetary-investing-passengers.trycloudflare.com";
+export const dominio = "https://zelo-server.com.br";
 export const apiGoogle = "AIzaSyCQvbNhFUQJI31Zflx_h3wxLqTd70GE13o";
 export const ambient_firebase = {
     apiKey: "AIzaSyBYK20Z_BR4TnpM27C1Hm-M-aI7o51oEIg",
