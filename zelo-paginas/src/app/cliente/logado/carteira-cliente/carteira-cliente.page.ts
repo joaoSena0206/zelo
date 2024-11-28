ca25ab7fe40489804d4c8e9f0c0d41471a16457d
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-11-27 23:41:21 -0300

    adiciona carteira cliente

---
+++ zelo-paginas/src/app/cliente/logado/carteira-cliente/carteira-cliente.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-carteira-cliente',
+  templateUrl: './carteira-cliente.page.html',
+  styleUrls: ['./carteira-cliente.page.scss'],
+})
+export class CarteiraClientePage implements OnInit {
+
+  constructor(private navCl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  voltarPag() {
+    this.navCl.back();
+  }
+
+}
