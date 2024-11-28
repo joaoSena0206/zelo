450936dbde796a559056ede2c387f7a3d2420940
João Sena <joaosena0206@gmail.com>
2024-11-28 18:05:58 -0300

    Formata melhor

--- zelo-paginas/src/app/cliente/logado/carteira/carteira-cliente/carteira-cliente.page.ts
+++ zelo-paginas/src/app/cliente/logado/carteira/carteira-cliente/carteira-cliente.page.ts
@@ -2,31 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-carteira-cliente',
-  templateUrl: './carteira-cliente.page.html',
-  styleUrls: ['./carteira-cliente.page.scss'],
+    selector: 'app-carteira-cliente',
+    templateUrl: './carteira-cliente.page.html',
+    styleUrls: ['./carteira-cliente.page.scss'],
 })
 export class CarteiraClientePage implements OnInit {
 
-  constructor(private navCl: NavController) { }
+    constructor(private navCl: NavController) { }
 
-  ngOnInit() {
-  }
-
-  voltarPag() {
-    this.navCl.back();
-  }
-
-  marcogay(valor: any) {
-    let modal = document.querySelector("#example-modal") as HTMLIonModalElement;
-    if(valor != "custom-checked")
-    {
-      localStorage.setItem("ValorDepositarCarteira", valor);
-      this.navCl.navigateRoot("/pagamento-carteira");
-
-      modal.dismiss();
+    ngOnInit() {
     }
-  }
+
+    voltarPag() {
+        this.navCl.back();
+    }
+
+    marcogay(valor: any) {
+        let modal = document.querySelector("#example-modal") as HTMLIonModalElement;
+        if (valor != "custom-checked") {
+            localStorage.setItem("ValorDepositarCarteira", valor);
+            this.navCl.navigateRoot("/pagamento-carteira");
+
+            modal.dismiss();
+        }
+    }
 
 
 
