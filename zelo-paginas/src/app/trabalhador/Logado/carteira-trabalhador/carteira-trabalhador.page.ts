cbe040cc1909f042643a4a99d84bd8e4a33c9a49
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-11-27 23:42:55 -0300

    adiciona carteira

---
+++ zelo-paginas/src/app/trabalhador/Logado/carteira-trabalhador/carteira-trabalhador.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-carteira-trabalhador',
+  templateUrl: './carteira-trabalhador.page.html',
+  styleUrls: ['./carteira-trabalhador.page.scss'],
+})
+export class CarteiraTrabalhadorPage implements OnInit {
+
+  constructor(private navCl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  voltarPag() {
+    this.navCl.back();
+  }
+
+}
