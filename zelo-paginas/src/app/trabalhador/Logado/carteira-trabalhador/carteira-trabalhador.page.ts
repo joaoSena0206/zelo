afc7a755d15f46dda9fad7a330758c54b000821d
João Sena <joaosena0206@gmail.com>
2024-11-30 15:23:25 -0300

    Faz a requisição para pegar as transações

--- zelo-paginas/src/app/trabalhador/Logado/carteira-trabalhador/carteira-trabalhador.page.ts
+++ zelo-paginas/src/app/trabalhador/Logado/carteira-trabalhador/carteira-trabalhador.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { dominio } from 'src/app/gerais';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-carteira-trabalhador',
@@ -8,14 +11,35 @@ import { NavController } from '@ionic/angular';
 })
 export class CarteiraTrabalhadorPage implements OnInit {
     trabalhador: any = JSON.parse(localStorage.getItem("trabalhador")!);
+    saldo: any = Number(localStorage.getItem("saldoCarteira"));
+    carregar: boolean = false;
+    transacoes: any;
 
-    constructor(private navCl: NavController) { }
+    constructor(private navCl: NavController, private http: HttpClient) { }
 
     ngOnInit() {
+        this.carregarTransacoes();
     }
 
     voltarPag() {
         this.navCl.navigateRoot("/trabalhador/inicial");
     }
 
+    async carregarTransacoes() {
+        try {
+            let link = dominio + `/TransacaoCarteira/CarregarTransacoes?cpf=${this.trabalhador.Cpf}&tipo=trabalhador`;
+            this.carregar = true;
+            let res: any = await firstValueFrom(this.http.get(link));
+
+            this.transacoes = res;
+        }
+        catch {
+            const alert = document.querySelector("ion-alert") as HTMLIonAlertElement;
+            alert.message = "Erro ao conectar-se ao servidor";
+            alert.present();
+        }
+        finally {
+            this.carregar = false;
+        }
+    }
 }
