a4ee3b7490240e6a74c38c0239622593ceebd3c2
João Sena <joaosena0206@gmail.com>
2024-11-30 10:03:33 -0300

    Adiciona modelo da transação

---
+++ zelo-core/Models/TransacaoCarteira.cs
@@ -0,0 +1,13 @@
+﻿public class TransacaoCarteira
+{
+    public int Codigo { get; set; }
+    public Cliente? Cliente { get; set; }
+    public Trabalhador? Trabalhador { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime Data { get; set; }
+
+    public TransacaoCarteira()
+    {
+
+    }
+}
