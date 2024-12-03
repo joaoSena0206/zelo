c65730511cd845eb5742b7f19661aa8fc419beb3
MarcoJuino <136519016+MarcoJuino@users.noreply.github.com>
2024-12-03 00:26:52 -0300

    atualiza ts de avaliacao

--- zelo-paginas/src/app/cliente/logado/servico/avaliacao/avaliacao.page.ts
+++ zelo-paginas/src/app/cliente/logado/servico/avaliacao/avaliacao.page.ts
@@ -3,6 +3,7 @@ import { dominio, headerNgrok } from 'src/app/gerais';
 import { first, firstValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { NavController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-avaliacao',
@@ -12,10 +13,10 @@ import { NavController, ToastController } from '@ionic/angular';
 export class AvaliacaoPage implements OnInit {
     cliente: any = JSON.parse(localStorage.getItem("cliente")!);
 
-    constructor(private http: HttpClient, private navCl: NavController) { }
+    constructor(private http: HttpClient, private navCl: NavController, private toastController: ToastController, private router: Router) { }
 
     async ngOnInit() {
-        this.carregarEstrelas(5);
+        this.carregarEstrelas(0);
 
         let link = dominio + "/TransacaoCarteira/AdicionarTransacao";
         let dadosForm = new FormData();
@@ -65,13 +66,31 @@ export class AvaliacaoPage implements OnInit {
 
         let dadosForm = new FormData();
         dadosForm.append("tipo", "servico");
+
+        if(comentario.value == "")
+        {
+            dadosForm.append("comentario", null!);
+        }
+
+        if(this.EstrelaSelecionada == 0)
+        {
+            let erro = document.querySelector(".erro") as HTMLIonAlertElement;
+            erro.classList.remove("escondido");
+            return;
+        }
+        else
+        {
+            let erro = document.querySelector(".erro") as HTMLIonAlertElement;
+            erro.classList.add("escondido");
+        }
+
         dadosForm.append("comentario", comentario.value?.toString()!);
         dadosForm.append("estrelas", this.EstrelaSelecionada);
         dadosForm.append("cdServico", this.cdSolicitacao.CdSolicitacaoServico);
 
         try {
             this.carregar = true;
-            let resposta = await firstValueFrom(this.http.post(link, dadosForm, { headers: headerNgrok }));
+            let resposta = await firstValueFrom(this.http.post(link, dadosForm));
 
             localStorage.removeItem("servico");
             localStorage.removeItem("endereco");
@@ -93,6 +112,25 @@ export class AvaliacaoPage implements OnInit {
 
     voltarPag() {
         this.navCl.navigateBack("/inicial")
+    }
+
+    ionViewDidLeave() {
+        const nextUrl = this.router.url;
+
+        if (nextUrl === '/inicial') {
+            this.showTemporaryToast();
+        }
+    }
+
+    async showTemporaryToast() {
+        const toast = await this.toastController.create({
+          message: 'Pedido completo com sucesso!',
+          duration: 2000,
+          position: 'top',
+          cssClass: 'custom-toast',
+        });
+    
+        await toast.present();
     }
 
 }
